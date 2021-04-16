5e0447d6f6494d64a3548ad04c7f273adc60921b
Hefan <s1977764@ed.ac.uk>
2021-04-17 00:16:11 +0800

    Hefan: updated CateringCompanyImp and SupermarketImp

--- src/main/java/shield/SupermarketClientImp.java
+++ src/main/java/shield/SupermarketClientImp.java
@@ -4,39 +4,79 @@
 
 package shield;
 
+import java.io.IOException;
+
 public class SupermarketClientImp implements SupermarketClient {
+  private String endpoint;
+  private String name;
+  private String postCode;
+  private boolean isRegistered;
   public SupermarketClientImp(String endpoint) {
   }
 
   @Override
   public boolean registerSupermarket(String name, String postCode) {
-    return false;
+    String request = "/registerSupermarket?business_name=" + name + "&postcode=" + postCode;
+    try {
+      ClientIO.doGETRequest(endpoint+request);
+      this.name = name;
+      this.postCode = postCode;
+      this.isRegistered = true;
+      return true;
+    } catch (IOException e) {
+      e.printStackTrace();
+      return false;
+    }
   }
 
   // **UPDATE2** ADDED METHOD
   @Override
   public boolean recordSupermarketOrder(String CHI, int orderNumber) {
-    return false;
+    String request = "recordSupermarketOrder?individual_id=" + CHI + "&order_number=" + orderNumber + "&supermarket_business_name=" + name + "&supermarket_postcode=" + postCode;
+    try {
+      String respond = ClientIO.doGETRequest(endpoint+request);
+      if(respond.equals("True")){
+        return true;
+      }
+      else{
+        return false;
+      }
+    } catch (IOException e) {
+      e.printStackTrace();
+      return false;
+    }
   }
 
   // **UPDATE**
   @Override
   public boolean updateOrderStatus(int orderNumber, String status) {
-    return false;
+    String request = "/updateSupermarketOrderStatus?order_id=" + orderNumber + "&newStatus=" + status;
+    try {
+      String respond = ClientIO.doGETRequest(endpoint+request);
+      if(respond.equals("True")){
+        return true;
+      }
+      else{
+        return false;
+      }
+    } catch (IOException e) {
+      e.printStackTrace();
+      return false;
+    }
   }
 
   @Override
   public boolean isRegistered() {
-    return false;
+    return isRegistered;
   }
 
   @Override
   public String getName() {
-    return null;
+    return name;
   }
 
   @Override
   public String getPostCode() {
-    return null;
+    return postCode;
   }
 }
