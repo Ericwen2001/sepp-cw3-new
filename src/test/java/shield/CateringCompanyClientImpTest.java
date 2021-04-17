e44438a4a2c8b75a8b0bd38f66ecd24d761f7abd
Hefan <s1977764@ed.ac.uk>
2021-04-17 23:09:17 +0800

    updated tests for company and supermarket

--- src/test/java/shield/CateringCompanyClientImpTest.java
+++ src/test/java/shield/CateringCompanyClientImpTest.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.io.IOException;
 import java.util.Collection;
 import java.util.Properties;
 import java.time.LocalDateTime;
@@ -24,6 +25,7 @@ public class CateringCompanyClientImpTest {
 
   private Properties clientProps;
   private CateringCompanyClient client;
+  private int testOrderNumber;
 
   private Properties loadProperties(String propsFilename) {
     ClassLoader loader = Thread.currentThread().getContextClassLoader();
@@ -44,6 +46,31 @@ public class CateringCompanyClientImpTest {
     clientProps = loadProperties(clientPropsFilename);
 
     client = new CateringCompanyClientImp(clientProps.getProperty("endpoint"));
+    Random rand = new Random();
+    String name = String.valueOf(rand.nextInt(10000));
+    String postCode = "EH8_5FL";
+    String chi = String.valueOf(42);
+
+    try {
+      String respond = ClientIO.doGETRequest(clientProps.getProperty("endpoint") + "/registerShieldingIndividual?CHI=" + chi);
+      System.out.println(respond);
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+
+    try {
+      ClientIO.doGETRequest(clientProps.getProperty("endpoint") + "/registerCateringCompany?business_name=" + name + "&postcode=" + postCode);
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+
+    try {
+      testOrderNumber = Integer.parseInt(ClientIO.doPOSTRequest(clientProps.getProperty("endpoint") + "/placeOrder?individual_id=" +
+                      chi + "&catering_business_name=" + name + "&catering_postcode=" + postCode,
+              "{\"contents\": [{\"id\":1,\"name\":\"cucumbers\",\"quantity\":20},{\"id\":2,\"name\":\"tomatoes\",\"quantity\":2}]}"));
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
   }
 
 
@@ -54,7 +81,15 @@ public class CateringCompanyClientImpTest {
     String postCode = "EH8_5FL";
 
     assertTrue(client.registerCateringCompany(name, postCode));
-//    assertTrue(client.isRegistered());
-//    assertEquals(client.getName(), name);
+    assertTrue(client.isRegistered());
+    assertEquals(client.getName(), name);
+  }
+
+  @Test
+  public void testCateringCompanyUpdateOrderStatus() {
+    assertTrue(client.updateOrderStatus(testOrderNumber, "packed"));
+    assertTrue(client.updateOrderStatus(testOrderNumber, "dispatched"));
+    assertTrue(client.updateOrderStatus(testOrderNumber, "delivered"));
+    assertTrue(!client.updateOrderStatus(testOrderNumber, "gibberish"));
   }
 }
--- src/test/java/shield/SupermarketClientImpTest.java
+++ src/test/java/shield/SupermarketClientImpTest.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.io.IOException;
 import java.util.Collection;
 import java.util.Properties;
 import java.time.LocalDateTime;
@@ -24,6 +25,11 @@ public class SupermarketClientImpTest {
 
   private Properties clientProps;
   private SupermarketClient client;
+  private Random rand = new Random();
+  private int testOrderNumber = rand.nextInt(10000);
+  private String name = String.valueOf(rand.nextInt(10000));
+  private String postCode = "EH8_5FL";
+  private String chi = String.valueOf(42);
 
   private Properties loadProperties(String propsFilename) {
     ClassLoader loader = Thread.currentThread().getContextClassLoader();
@@ -44,6 +50,29 @@ public class SupermarketClientImpTest {
     clientProps = loadProperties(clientPropsFilename);
 
     client = new SupermarketClientImp(clientProps.getProperty("endpoint"));
+
+    try {
+      String respond = ClientIO.doGETRequest(clientProps.getProperty("endpoint") + "/registerShieldingIndividual?CHI=" + chi);
+      System.out.println(respond);
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+
+    try {
+      ClientIO.doGETRequest(clientProps.getProperty("endpoint") + "/registerSupermarket?business_name=" + name + "&postcode=" + postCode);
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+    client.setName(name);
+    client.setPostCode(postCode);
+
+//    try {
+//      testOrderNumber = Integer.parseInt(ClientIO.doPOSTRequest(clientProps.getProperty("endpoint") + "/placeOrder?individual_id=" +
+//                      chi + "&catering_business_name=" + name + "&catering_postcode=" + postCode,
+//              "{\"contents\": [{\"id\":1,\"name\":\"cucumbers\",\"quantity\":20},{\"id\":2,\"name\":\"tomatoes\",\"quantity\":2}]}"));
+//    } catch (IOException e) {
+//      e.printStackTrace();
+//    }
   }
 
 
@@ -56,5 +85,18 @@ public class SupermarketClientImpTest {
     assertTrue(client.registerSupermarket(name, postCode));
     assertTrue(client.isRegistered());
     assertEquals(client.getName(), name);
+  }
+
+  @Test
+  public void testSupermarketUpdateOrderStatus() {
+    assertTrue(!client.updateOrderStatus(testOrderNumber, "packed"));
+    assertTrue(!client.updateOrderStatus(testOrderNumber, "dispatched"));
+    assertTrue(!client.updateOrderStatus(testOrderNumber, "delivered"));
+    assertTrue(!client.updateOrderStatus(testOrderNumber, "gibberish"));
+  }
+
+  @Test
+  public void testRecordSupermarketOrder() {
+    assertTrue(client.recordSupermarketOrder(chi, testOrderNumber));
   }
 }
