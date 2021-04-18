1e30c8363cc02213294cd1dfe28d812feb277aa5
Hefan <s1977764@ed.ac.uk>
2021-04-18 19:37:48 +0800

    added comments and argument checks for company and supermarket

--- src/main/java/shield/SupermarketClientImp.java
+++ src/main/java/shield/SupermarketClientImp.java
@@ -5,6 +5,7 @@
 package shield;
 
 import java.io.IOException;
+import java.util.Objects;
 
 public class SupermarketClientImp implements SupermarketClient {
   private String endpoint;
@@ -15,8 +16,16 @@ public class SupermarketClientImp implements SupermarketClient {
     this.endpoint = endpoint;
   }
 
+  /**
+   * Register the business to the server. At the same time initialize the object.
+   * @param name name of the business
+   * @param postCode post code of the business
+   * @return true if the business is successfully registered.
+   */
   @Override
   public boolean registerSupermarket(String name, String postCode) {
+    Objects.requireNonNull(name);
+    Objects.requireNonNull(postCode);
     String request = "/registerSupermarket?business_name=" + name + "&postcode=" + postCode;
     try {
       ClientIO.doGETRequest(endpoint+request);
@@ -31,11 +40,23 @@ public class SupermarketClientImp implements SupermarketClient {
   }
 
   // **UPDATE2** ADDED METHOD
+
+  /**
+   * This method is for supermarkets to record a target order.
+   * @param CHI CHI number of the shielding individual associated with this order
+   * @param orderNumber the order number
+   * @return true if the target order is successfully recorded.
+   */
   @Override
   public boolean recordSupermarketOrder(String CHI, int orderNumber) {
+    Objects.requireNonNull(CHI);
+    Objects.requireNonNull(orderNumber);
+    Objects.requireNonNull(name);
+    Objects.requireNonNull(postCode);
     String request = "/recordSupermarketOrder?individual_id=" + CHI + "&order_number=" + orderNumber + "&supermarket_business_name=" + name + "&supermarket_postcode=" + postCode;
     try {
       String respond = ClientIO.doGETRequest(endpoint+request);
+      System.out.println(respond);
       if(respond.equals("True")){
         return true;
       }
@@ -49,8 +70,16 @@ public class SupermarketClientImp implements SupermarketClient {
   }
 
   // **UPDATE**
+  /**
+   * Update the target order to the target status. Status can only be one of "packed/dispatched/delivered".
+   * @param orderNumber the order number
+   * @param status status of the order for the requested number
+   * @return true if the status of the order is successfully updated.
+   */
   @Override
   public boolean updateOrderStatus(int orderNumber, String status) {
+    Objects.requireNonNull(orderNumber);
+    Objects.requireNonNull(status);
     String request = "/updateSupermarketOrderStatus?order_id=" + orderNumber + "&newStatus=" + status;
     try {
       String respond = ClientIO.doGETRequest(endpoint+request);
@@ -66,6 +95,9 @@ public class SupermarketClientImp implements SupermarketClient {
     }
   }
 
+  /**
+   * @return whether the business using this client is registered.
+   */
   @Override
   public boolean isRegistered() {
     return isRegistered;
